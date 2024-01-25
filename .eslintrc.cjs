9dabb15dd07e4b2c34c84296661ec4b7165bd811
Chaeil Yun <chaeily@andrew.cmu.edu>
2024-01-25 03:03:29 -0500

    added the cjs file to commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -63,7 +63,13 @@ function find_compiled_js() {
                 },
                 rules: {
                     "no-use-before-define": "off",
-                    "@typescript-eslint/no-use-before-define": "error",			
+                    "@typescript-eslint/no-use-before-define": "error",	
+                    "@typescript-eslint/no-misused-promises": [
+                        "error",
+                        {
+                          "checksVoidReturn": false
+                        }
+                      ]		
                 }
             }
         ]
